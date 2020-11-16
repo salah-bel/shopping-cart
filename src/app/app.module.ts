98eb379265dfd82d25546447588c1e8813664bee
Salah Bel <belasalah@gmail.com>
2020-11-16 19:20:34 +0100

    ui design cart interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent  } from './app.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { FilterComponent } from './components/shopping-cart/filter/filter.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { TimerComponent } from './timer/timer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CartComponent,
+    FilterComponent,
+    ShoppingCartComponent,
+    TimerComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    ProductListComponent,
+    CartItemComponent,
+    ProductItemComponent
+    
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
+    
   ],
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
